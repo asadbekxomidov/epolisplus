9761e645c7d4a201f0eedb34d2808ea1f567dafd
asadbekxomidov <asadbekxomidov2006@gmail.com>
2024-10-13 16:02:18 +0500

    a14

--- lib/utils/my_function.dart
+++ lib/utils/my_function.dart
@@ -14,11 +14,3 @@ clearPhoneMask(String phoneNumber) {
   phoneNumber = phoneNumber.replaceAll(" ", "");
   return phoneNumber;
 }
-
-clearPasswordMask(String password) {
-  password = password.replaceAll("(", "");
-  password = password.replaceAll(")", "");
-  password = password.replaceAll("-", "");
-  password = password.replaceAll(" ", "");
-  return password;
-}
