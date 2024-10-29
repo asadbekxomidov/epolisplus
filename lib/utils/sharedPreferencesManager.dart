fef21dc5a468b2823750974f06d9453546375ecc
asadbekxomidov <asadbekxomidov2006@gmail.com>
2024-10-29 12:18:17 +0500

    sharedPreferencesManager.dart file update

--- lib/utils/sharedPreferencesManager.dart
+++ lib/utils/sharedPreferencesManager.dart
@@ -1,27 +1,3 @@
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class SharedPreferencesManager {
-//   static const _tokenKey = 'access_token';
-
-//   // Tokenni saqlash
-//   Future<void> saveToken(String token) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(_tokenKey, token);
-//   }
-
-//   // Tokenni olish
-//   Future<String?> getToken() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(_tokenKey);
-//   }
-
-//   // Tokenni o'chirish (logout yoki muddati o'tgan bo'lsa)
-//   Future<void> clearToken() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.remove(_tokenKey);
-//   }
-// }
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SharedPreferencesManager {
